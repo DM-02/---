9adf85c337772af5547355f9dba5aaa7a2138ab0
Dmitry <142209152+DM-02@users.noreply.github.com>
2024-02-21 19:13:14 +0500

    Update main.cpp

--- Классы-коллекции/main.cpp
+++ Классы-коллекции/main.cpp
@@ -6,9 +6,54 @@
 //
 
 #include <iostream>
+#include <cstring>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+class String {
+private:
+    const static int MAX_LENGTH = 80;
+    char* str;
+    
+public:
+    String() {
+        str = new char[MAX_LENGTH + 1];
+        str[0] = '\0';
+    }
+    
+    String(int length) {
+        str = new char[length + 1];
+        str[0] = '\0';
+    }
+    
+    String(const char* s) {
+        str = new char[strlen(s) + 1];
+        strcpy(str, s);
+    }
+    
+    void inputString() {
+        std::cout << "Enter a string: ";
+        std::cin.getline(str, MAX_LENGTH);
+    }
+    
+    void displayString() {
+        std::cout << "String: " << str << std::endl;
+    }
+    
+    ~String() {
+        delete[] str;
+    }
+};
+
+int main() {
+    String defaultString;
+    String customLengthString(50);
+    
+    String userInputString;
+    userInputString.inputString();
+    
+    defaultString.displayString();
+    customLengthString.displayString();
+    userInputString.displayString();
+    
     return 0;
 }
+
